63dc0918d69e2c8fe1d5057328bc95fe6fdc9860
llm <luoqin123@qq.com>
2020-12-22 15:52:21 +0800

    Initial commit

---
+++ springboot_demo1/src/test/java/com/jt/SpringbootDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
